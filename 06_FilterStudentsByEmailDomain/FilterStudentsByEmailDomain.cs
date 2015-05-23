cf90e026750f0c58a30f9b56e0aaa08d270f1e34
nstanevski <nikolay.stanevski@gmail.com>
2015-05-23 22:38:39 +0300

    task #06 done

---
+++ 06_FilterStudentsByEmailDomain/FilterStudentsByEmailDomain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Print all students that have email @abv.bg. Use LINQ.
+
+class FilterStudentsByEmailDomain
+{
+    static void Main()
+    {
+        List<Student> studList = StudentsList.studentsList;
+        var studentQry =
+            from student in studList
+            where student.Email.IndexOf("@abv.bg") > 0
+            orderby student.FirstName
+            select student;
+
+        Console.WriteLine("* STUDENTS WITH EMAIL AT DOMAIN @abv.bg *");
+        foreach (Student student in studentQry)
+        {
+            Console.WriteLine("First name: {0}", student.FirstName);
+            Console.WriteLine("Last name: {0}", student.LastName);
+            Console.WriteLine("Age: {0}", student.Age);
+            Console.WriteLine("Faculty number: {0}", student.FacultyNumber);
+            Console.WriteLine("Phone: {0}", student.Phone);
+            Console.WriteLine("Email: {0}", student.Email);
+            Console.WriteLine("Marks: {0}", String.Join(", ", student.Marks.ToArray()));
+            Console.WriteLine("Group Number: {0}", student.GroupNumber);
+            Console.WriteLine("=========================================");
+        }
+    }
+}
