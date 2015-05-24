24e707ace5aa31d9af0b2430d955bf5777b73f49
nstanevski <nikolay.stanevski@gmail.com>
2015-05-24 07:23:59 +0300

    task#8

---
+++ 08_ExcellentStudents/ExcellentStudents.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+ * Print all students that have at least one mark Excellent (6). Using LINQ first select them
+ * into a new anonymous class that holds { FullName + Marks}.
+ */
+
+class ExcellentStudents
+{
+    static void Main()
+    {
+        List<Student> studList = StudentsList.studentsList;
+        var studentQry =
+            from student in studList
+            where student.Marks.IndexOf(6)>-1
+            orderby student.FirstName
+            select new
+            {
+                FullName = student.FirstName + " "+student.LastName,
+                Marks = string.Join(", ", student.Marks.ToArray())
+            };
+
+
+        Console.WriteLine("*********** STUDENTS WITH EXCELLENT MARKS ***********");
+        foreach (var data in studentQry)
+        {
+            Console.WriteLine("Name: {0}", data.FullName);
+            Console.WriteLine("Marks: {0}", data.Marks);
+            
+            Console.WriteLine("=========================================");
+        }
+    }
+}
---
+++ 08_ExcellentStudents/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Create a class Student with properties FirstName, LastName, Age, FacultyNumber, Phone, 
+ * Email, Marks (IList<int>), GroupNumber.
+ */
+
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string FacultyNumber { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public IList<int> Marks { get; set; }
+    public int GroupNumber { get; set; }
+}
